1ee1dda73831798f2af6c277c57b67b923386887
lucas-delazeri <lucasdelazeris@gmail.com>
2026-09-16 11:30:35 -0300

    feat: lesson 08 computer graphics

---
+++ Aula-08/2/main.cpp
@@ -0,0 +1,284 @@
+// Copyright
+// Computação Gráfica
+// URI Santiago
+// Professor Laurence
+
+#define STB_IMAGE_IMPLEMENTATION
+
+#include <GL/glew.h>
+#include <GL/glu.h>
+#include <GLFW/glfw3.h>
+
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <sstream>
+
+#include "stb_image.h"
+
+const GLfloat kCameraSpeed = 0.1;
+const GLfloat kMouseSensitivity = 0.1;
+
+const GLfloat kDefaultCameraEyePositionX = 0;
+const GLfloat kDefaultCameraEyePositionY = 0;
+const GLfloat kDefaultCameraEyePositionZ = 0;
+GLfloat camera_eye_x = kDefaultCameraEyePositionX;
+GLfloat camera_eye_y = kDefaultCameraEyePositionY;
+GLfloat camera_eye_z = kDefaultCameraEyePositionZ;
+
+const GLfloat kDefaultCameraDirectionX = 0;
+const GLfloat kDefaultCameraDirectionY = 0;
+const GLfloat kDefaultCameraDirectionZ = -1;
+GLfloat camera_direction_x = kDefaultCameraDirectionX;
+GLfloat camera_direction_y = kDefaultCameraDirectionY;
+GLfloat camera_direction_z = kDefaultCameraDirectionZ;
+
+const GLfloat kCameraUpX = 0;
+const GLfloat kCameraUpY = 1;
+const GLfloat kCameraUpZ = 0;
+
+GLfloat camera_right_x;
+GLfloat camera_right_y;
+GLfloat camera_right_z;
+
+const GLfloat kDefaultCameraYaw = -90;
+GLfloat camera_yaw = kDefaultCameraYaw;
+
+const GLfloat kCameraPitchLimit = 90;
+const GLfloat kDefaultCameraPitch = 0;
+GLfloat camera_pitch = kDefaultCameraPitch;
+
+const GLfloat kPerspectiveFieldOfViewAngle = 45;
+const GLfloat kPerspectiveNearZ = 0.1;
+const GLfloat kPerspectiveFarZ = 100;
+const GLfloat kPerspectiveTranslateZ = 10;
+
+const GLint kSkyColorRed = 25;
+const GLint kSkyColorGreen = 118;
+const GLint kSkyColorBlue = 210;
+
+const GLint kGroundColorRed = 63;
+const GLint kGroundColorGreen = 132;
+const GLint kGroundColorBlue = 26;
+
+const GLfloat kTreePositionX = 0;
+const GLfloat kTreePositionY = 0;
+const GLfloat kTreePositionZ = -10;
+GLuint tree_texture_id;
+
+void load_texture(GLuint& texture_id, const std::string& filepath) {
+  int texture_width, texture_height, texture_channels;
+  uint8_t* texture_data =
+      stbi_load(filepath.c_str(), &texture_width, &texture_height, &texture_channels, 0);
+
+  if (!texture_data) {
+    std::cerr << "Erro ao carregar a textura: " << filepath << std::endl;
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  glGenTextures(1, &texture_id);
+  glBindTexture(GL_TEXTURE_2D, texture_id);
+  GLenum texture_format = ((texture_channels == 4) ? GL_RGBA : GL_RGB);
+  glTexImage2D(GL_TEXTURE_2D, 0, texture_format, texture_width, texture_height, 0, texture_format,
+               GL_UNSIGNED_BYTE, texture_data);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  stbi_image_free(texture_data);
+}
+
+static void mouse_read(GLFWwindow* window, double mouse_x, double mouse_y) {
+  static bool mouse_initialized = false;
+  static GLfloat last_mouse_x = 0, last_mouse_y = 0;
+  if (!mouse_initialized) {
+    last_mouse_x = mouse_x;
+    last_mouse_y = mouse_y;
+    mouse_initialized = true;
+  }
+
+  GLfloat mouse_delta_x = (mouse_x - last_mouse_x);
+  GLfloat mouse_delta_y = (last_mouse_y - mouse_y);
+  last_mouse_x = mouse_x;
+  last_mouse_y = mouse_y;
+
+  camera_pitch += (mouse_delta_y * kMouseSensitivity);
+  if (camera_pitch > kCameraPitchLimit) {
+    camera_pitch = kCameraPitchLimit;
+  }
+  if (camera_pitch < -kCameraPitchLimit) {
+    camera_pitch = -kCameraPitchLimit;
+  }
+  camera_yaw += (mouse_delta_x * kMouseSensitivity);
+
+  GLfloat camera_pitch_radians = camera_pitch * (3.1415 / 180);
+  GLfloat camera_yaw_radians = camera_yaw * (3.1415 / 180);
+  camera_direction_x = cosf(camera_yaw_radians) * cosf(camera_pitch_radians);
+  camera_direction_y = sinf(camera_pitch_radians);
+  camera_direction_z = sinf(camera_yaw_radians) * cosf(camera_pitch_radians);
+
+  camera_right_x = camera_direction_z * kCameraUpY - camera_direction_y * kCameraUpZ;
+  camera_right_y = camera_direction_x * kCameraUpZ - camera_direction_z * kCameraUpX;
+  camera_right_z = camera_direction_y * kCameraUpX - camera_direction_x * kCameraUpY;
+}
+
+void keyboard_read(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, true);
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+    camera_eye_x += camera_direction_x * kCameraSpeed;
+    camera_eye_y += camera_direction_y * kCameraSpeed;
+    camera_eye_z += camera_direction_z * kCameraSpeed;
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+    camera_eye_x -= camera_direction_x * kCameraSpeed;
+    camera_eye_y -= camera_direction_y * kCameraSpeed;
+    camera_eye_z -= camera_direction_z * kCameraSpeed;
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+    camera_eye_x += camera_right_x * kCameraSpeed;
+    camera_eye_y += camera_right_y * kCameraSpeed;
+    camera_eye_z += camera_right_z * kCameraSpeed;
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+    camera_eye_x -= camera_right_x * kCameraSpeed;
+    camera_eye_y -= camera_right_y * kCameraSpeed;
+    camera_eye_z -= camera_right_z * kCameraSpeed;
+  }
+
+  if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+    camera_eye_x = kDefaultCameraEyePositionX;
+    camera_eye_y = kDefaultCameraEyePositionY;
+    camera_eye_z = kDefaultCameraEyePositionZ;
+    camera_direction_x = kDefaultCameraDirectionX;
+    camera_direction_y = kDefaultCameraDirectionY;
+    camera_direction_z = kDefaultCameraDirectionZ;
+    camera_yaw = kDefaultCameraYaw;
+    camera_pitch = kDefaultCameraPitch;
+  }
+}
+
+void resize_window(GLFWwindow* window) {
+  int window_width, window_height;
+  glfwGetFramebufferSize(window, &window_width, &window_height);
+  glViewport(0, 0, window_width, window_height);
+}
+
+void draw(GLFWwindow* window) {
+  int window_width, window_height;
+  glfwGetFramebufferSize(window, &window_width, &window_height);
+  GLdouble aspect_ratio = (GLdouble)window_width / window_height;
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  GLdouble field_of_view = kPerspectiveFieldOfViewAngle;
+  GLdouble near = kPerspectiveNearZ;
+  GLdouble far = kPerspectiveFarZ;
+  gluPerspective(field_of_view, aspect_ratio, near, far);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+  GLfloat camera_center_x = (camera_eye_x + camera_direction_x);
+  GLfloat camera_center_y = (camera_eye_y + camera_direction_y);
+  GLfloat camera_center_z = (camera_eye_z + camera_direction_z);
+  gluLookAt(camera_eye_x, camera_eye_y, camera_eye_z,           // eye
+            camera_center_x, camera_center_y, camera_center_z,  // direction
+            kCameraUpX, kCameraUpY, kCameraUpZ                  // up
+  );
+
+  glColor3ub(kGroundColorRed, kGroundColorGreen, kGroundColorBlue);
+  glBegin(GL_QUADS);
+  {
+    glVertex3f(-25, -2.5, -25);
+    glVertex3f(25, -2.5, -25);
+    glVertex3f(25, -2.5, 25);
+    glVertex3f(-25, -2.5, 25);
+  }
+  glEnd();
+
+  glColor3ub(255, 255, 255);
+  glEnable(GL_TEXTURE_2D);
+  glPushMatrix();
+  {
+    glTranslatef(kTreePositionX, kTreePositionY, kTreePositionZ);
+    GLfloat tree_direction_angle_radians = atan2f(camera_direction_x, camera_direction_z);
+    GLfloat tree_direction_angle = tree_direction_angle_radians * (180.0 / 3.1415);
+    glRotatef(tree_direction_angle, 0, 1, 0);
+    glBindTexture(GL_TEXTURE_2D, tree_texture_id);
+    glBegin(GL_QUADS);
+    {
+      glTexCoord2f(1, 0);
+      glVertex3f(-2.5, 2.5, 0);
+      glTexCoord2f(1, 1);
+      glVertex3f(-2.5, -2.5, 0);
+      glTexCoord2f(0, 1);
+      glVertex3f(2.5, -2.5, 0);
+      glTexCoord2f(0, 0);
+      glVertex3f(2.5, 2.5, 0);
+    }
+    glEnd();
+  }
+  glPopMatrix();
+  glDisable(GL_TEXTURE_2D);
+}
+
+int main() {
+  if (!glfwInit()) {
+    std::cerr << "Falha ao inicializar GLFW" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  GLFWwindow* window = glfwCreateWindow(800, 800, "", NULL, NULL);
+  if (!window) {
+    std::cerr << "Falha ao criar a janela GLFW" << std::endl;
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+  glfwSetWindowPos(window, 0, 0);
+  glfwSetWindowTitle(window, "Billboard e camera");
+  glfwMakeContextCurrent(window);
+
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetCursorPosCallback(window, mouse_read);
+
+  if (glewInit() != GLEW_OK) {
+    std::cerr << "Falha ao inicializar GLEW" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  glEnable(GL_DEPTH_TEST);
+  glEnable(GL_BLEND);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+  load_texture(tree_texture_id, "textures/tree.png");
+
+  GLfloat clear_color_red = (GLfloat)kSkyColorRed / 255;
+  GLfloat clear_color_green = (GLfloat)kSkyColorGreen / 255;
+  GLfloat clear_color_blue = (GLfloat)kSkyColorBlue / 255;
+  GLfloat clear_color_alpha = 1;
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(clear_color_red, clear_color_green, clear_color_blue, clear_color_alpha);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    keyboard_read(window);
+    resize_window(window);
+    draw(window);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return EXIT_SUCCESS;
+}
